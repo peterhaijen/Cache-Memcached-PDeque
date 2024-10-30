0049ffe9aba2cd793a5a453845cf87628b6e336f
Peter Haijen <peter.haijen@gmail.com>
2024-10-30 18:37:25 +0100

    Version 0.03

--- lib/Cache/Memcached/PDeque.pm
+++ lib/Cache/Memcached/PDeque.pm
@@ -17,11 +17,11 @@ Cache::Memcached::PDeque - Implements a priority deque using memcached as storag
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
